48a2d70f5584ea34bb28f869df90780f3003b6b0
MORJAN-CUN <janluy_moreno@cun.edu.co>
2023-06-22 16:56:13 -0500

    cambio de archivo css para plataforma de proyeccion social

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
   <!-- Styles -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <link rel="stylesheet" href="{{ asset('css/app.css?V='.now()->format('H.s')) }}">
-  <link rel="stylesheet" href="{{ asset('css/saberPro.css?V='.now()->format('H.s')) }}">
+  <link rel="stylesheet" href="{{ asset('css/proSocial.css?V='.now()->format('H.s')) }}">
   <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css?V='.now()->format('H.s')) }}">
   <link rel="stylesheet" href="{{ asset('css/files.css?V='.now()->format('H.s')) }}">
   <link rel="stylesheet" href="{{ asset('css/inputs.css?V='.now()->format('H.s')) }}">
